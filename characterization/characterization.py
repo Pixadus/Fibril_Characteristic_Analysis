2cbab2aa955000c2fe29403463ddf307b091eb7b
Parker Lamb <andromeda@outlook.ie>
2022-01-17 12:25:05 -0700

    Redesign of breadth calculations

--- characterization/characterization.py
+++ characterization/characterization.py
@@ -10,13 +10,21 @@ breadth and velocity values for each coordinate point on each fibril.
 """
 
 import argparse
+from contextlib import suppress
 import sys
 import numpy as np
 import cv2
+from scipy.__config__ import show
 from scipy.io import readsav
 from matplotlib import pyplot as plt
 from collections import OrderedDict
 from os.path import exists
+
+# Modify numpy print options
+np.set_printoptions(suppress=True)
+
+# Disable if you don't want to visualize width calculations
+show_width_calculations = True
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Characterize fibrils on a coordinate-by-coordinate basis. ")
@@ -57,17 +65,16 @@ with open(args.coordinate_file, newline='') as csvfile:
             coords[num] = np.append(coords[num], [np.array([x,y])], axis=0)
         # Structure of coords is: {fibril_id: np.array([x,y],[x,y], ...)}
 
- 
 # Get intensity and velocity on a per-coordinate basis
-# for key in coords.keys():
-#     coord_info = []
-#     for coordpair in coords[key]:
-#         x = int(coordpair[0])
-#         y = int(coordpair[1])
-#         intensity = core_map[y,x]
-#         velocity = vel_map[y,x]
-#         coord_info.append([coordpair[0],coordpair[1],intensity,velocity])
-#     coords[key] = coord_info
+for key in coords.keys():
+    coord_info = []
+    for coordpair in coords[key]:
+        x = int(coordpair[0])
+        y = int(coordpair[1])
+        intensity = core_map[y,x]
+        velocity = vel_map[y,x]
+        coord_info.append([coordpair[0],coordpair[1],intensity,velocity])
+    coords[key] = np.array(coord_info)
 
 # Getting the breadth of each fibril. 
 ## This technique will sharpen the image, slightly smooth out noise, then run cv2.Canny
@@ -89,39 +96,86 @@ def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
 width_map_cv2 = width_map*325
 width_map_cv2 = width_map_cv2.astype(np.uint8)
 
-# Create a much sharpened image, then blur it a bit to get rid of noise
+# Create a sharpened image, then blur it a bit to get rid of noise
 wm_sharp = unsharp_mask(width_map_cv2, amount=10.0)
 wm_sharp_gauss = cv2.GaussianBlur(wm_sharp, (5,5), 8.0)
 
 edges = cv2.Canny(wm_sharp_gauss, threshold1=260, threshold2=280, apertureSize=7)
-overlay = cv2.addWeighted(width_map_cv2, 0.7, edges, 0.4,0)
 
 # Test if Canny boundaries match OCCULT-identified fibrils
-fig, ax = plt.subplots()
-plt.imshow(overlay, origin='lower')
-for key in coords.keys():
-     x = coords[key][:,0]
-     y = coords[key][:,1]
-     plt.scatter(x,y, 1.0, '#ff0000')
-plt.show()
-
-# This works really well. More noise than others, but edges clearly defined. 
-
-# LSD test - this works really well, alternative to OCCULT-2
-#lsd = cv2.createLineSegmentDetector(0)
-#lines = lsd.detect(width_map_cv2_blur)[0] #Position 0 of the returned tuple are the detected lines
-#drawn_img = lsd.drawSegments(width_map_cv2,lines)
-#plt.imshow(drawn_img, origin="lower")
-#plt.show()
-#cv2.waitKey(0)
-
-# # Test coordinate alignment
-# fig, ax = plt.subplots(figsize=(15,15)) # 15 inch by 15 inches
-# plt.imshow(core_map, origin="lower")
+# overlay = cv2.addWeighted(width_map_cv2, 0.7, edges, 0.4,0)
+# fig, ax = plt.subplots()
+# plt.imshow(overlay, origin='lower')
 # for key in coords.keys():
 #     x = coords[key][:,0]
 #     y = coords[key][:,1]
-#     plt.scatter(x,y)
-#     plt.plot(x,y)
-#     fig.canvas.draw()
+#     plt.plot(x,y,markersize=1,linewidth=1, color='#ff0000')
 # plt.show()
+
+# REMOVE Set up test --
+overlay = cv2.addWeighted(width_map_cv2, 0.7, edges, 0.4,0)
+plt.imshow(overlay, origin='lower')
+for key in coords.keys():
+    x = coords[key][:,0]
+    y = coords[key][:,1]
+    plt.plot(x,y,markersize=1,linewidth=1, color='#ff0000')
+# --
+
+# Calculate breadth of fibril
+keys = list(coords)
+for key in coords.keys():
+    # Get a list of all coordinates in the fibril
+    coordinfo = list(coords[key])
+    # Counter to display every nth width segment
+    wctr = 0
+    for coord in coordinfo:
+        wctr +=1
+        # Get next and previous coordinates
+        nextcoord = next((i for i, val in enumerate(coordinfo) if np.all(val == coord)), -1)+1
+        prevcoord = next((i for i, val in enumerate(coordinfo) if np.all(val == coord)), -1)-1
+        if nextcoord > len(coordinfo)-1:
+            nextcoord = len(coordinfo)-1
+        if prevcoord < 0:
+            prevcoord = 0
+        nextcoord = coordinfo[nextcoord]
+        prevcoord = coordinfo[prevcoord]
+        # Calculate the slope at the coordinate
+        dx = nextcoord[1]-prevcoord[1]
+        dy = nextcoord[0]-prevcoord[0]
+        v = np.array([dx,dy])
+        # Get perpendicular vector
+        dp = np.empty_like(v)
+        dp[0] = -v[1]
+        dp[1] = v[0]
+        # Convert to unit vector
+        mag = np.sqrt(dp[0]**2+dp[1]**2)
+        dp[0] = dp[0]/mag
+        dp[1] = dp[1]/mag
+        # Array indices must be integers, rounding
+        dp[0] = round(dp[0])
+        dp[1] = round(dp[1])
+        # Variables to store x and y displacement vectors for width visualization
+        xs = []
+        ys = []
+        # Move in positive dp until we hit a Canny-identified edge
+        bp= 0
+        coord_offset = np.array([round(coord[1]),round(coord[0])])
+        while edges[coord_offset[0],coord_offset[1]] == 0:
+            bp+=1
+            xs.append(coord_offset[0])
+            ys.append(coord_offset[1])
+            coord_offset[0] = coord_offset[0]+dp[0]
+            coord_offset[1] = coord_offset[1]+dp[1]
+        # Move in negative dp until we hit a Canny-identified edge
+        bn = 0
+        coord_offset = np.array([round(coord[1]),round(coord[0])])
+        while edges[coord_offset[0],coord_offset[1]] == 0:
+            bn+=1
+            xs.append(coord_offset[0])
+            ys.append(coord_offset[1])
+            coord_offset[0] = coord_offset[0]-dp[0]
+            coord_offset[1] = coord_offset[1]-dp[1]
+        if (wctr % 2) == 0 and show_width_calculations:
+            plt.plot(ys,xs,markersize=1,linewidth=1, color='#a09516')
+if show_width_calculations:
+    plt.show()
